02db38df2c69ca911599eb9f88cb07915758e719
Giorgio Cremonesi <gcremonesi@eonegroup.it>
2026-09-11 16:27:39 +0200

    Aggiornamento logica invio mail

--- eacm.rpdettmat/webapp/ext/controller/SendPdfHelper.js
+++ eacm.rpdettmat/webapp/ext/controller/SendPdfHelper.js
@@ -13,32 +13,38 @@ sap.ui.define([
     // Il dataset non viene letto dalla tabella a video: si rimandano al backend i filtri attivi.
     var SERVICE_ROOT = "/sap/opu/odata4/eacm/ui_rpdettmat_b/srvd/eacm/ui_rpdettmat/0001";
 
+    function _buildOptionsModel() {
+        return new JSONModel({
+            DetailPrint: false,
+            IncludeBlocked: false,
+            IncludeAntMinReceived: false
+        });
+    }
 
-    function _openPrintOptionsDialog(oExtensionAPI) {
+    function _openSendOptionsDialog(oExtensionAPI) {
         return new Promise(function (resolve) {
-/*  Da verificare --------------------------------------------------
             var oModel = _buildOptionsModel();
             var oDialog = new Dialog({
-                title: "Opzioni di stampa",
+                title: "{i18n>dialogTitle}",
                 contentWidth: "26rem",
                 content: new VBox({
                     items: [
                         new CheckBox({
-                            text: "Stampa dettaglio",
+                            text: "{i18n>printDetailText}",
                             selected: "{/DetailPrint}"
                         }),
                         new CheckBox({
-                            text: "Includi bloccati",
+                            text: "{i18n>includeBlockedText}",
                             selected: "{/IncludeBlocked}"
                         }),
                         new CheckBox({
-                            text: "Ant./Min. ricevuti",
+                            text: "{i18n>antMinReceivedText}",
                             selected: "{/IncludeAntMinReceived}"
                         })
                     ]
                 }),
                 beginButton: new Button({
-                    text: "Conferma",
+                    text: "{i18n>confirmButtonText}",
                     type: "Emphasized",
                     press: function () {
                         resolve(oModel.getData());
@@ -46,7 +52,7 @@ sap.ui.define([
                     }
                 }),
                 endButton: new Button({
-                    text: "Annulla",
+                    text: "{i18n>cancelButtonText}",
                     press: function () {
                         resolve(null);
                         oDialog.close();
@@ -60,18 +66,58 @@ sap.ui.define([
             oDialog.setModel(oModel);
             oExtensionAPI.addDependent(oDialog);
             oDialog.open();
----  Da verificare -------------------------------------------------- */            
         });
     }
 
-    async function _sendPdfFromListReport(oExtensionAPI, mOptions) {
-/*  Da verificare --------------------------------------------------       
+    function _normalizeActiveFilters(oFilterInfo) {
+        if (!oFilterInfo) {
+            return [];
+        }
+
+        if (Array.isArray(oFilterInfo)) {
+            return oFilterInfo.filter(Boolean);
+        }
+
+        if (Array.isArray(oFilterInfo.filters)) {
+            return oFilterInfo.filters.filter(Boolean);
+        }
+
+        if (oFilterInfo.filters) {
+            return [oFilterInfo.filters];
+        }
+
+        if (oFilterInfo.filter) {
+            return [oFilterInfo.filter];
+        }
+
+        return [];
+    }
+
+    function _buildMailSenderFilters(oExtensionAPI, mOptions) {
+        var aFilters = _normalizeActiveFilters(
+            typeof oExtensionAPI.getFilters === "function"
+                ? oExtensionAPI.getFilters()
+                : null
+        );
+
+        if (!aFilters.length) {
+            throw new Error("Non è possibile eseguire l''invio mail senza aver indicato alcun filtro.");
+        }
+
+        aFilters.push(new Filter("DetailPrint", FilterOperator.EQ, !!mOptions.DetailPrint));
+        aFilters.push(new Filter("IncludeBlocked", FilterOperator.EQ, !!mOptions.IncludeBlocked));
+        aFilters.push(new Filter("IncludeAntMinReceived", FilterOperator.EQ, !!mOptions.IncludeAntMinReceived));
+
+        return aFilters;
+    }
+
+    async function _sendMailFromListReport(oExtensionAPI, mOptions) {
         var oModel = oExtensionAPI.getModel();
-        var aFilters = _buildPdfDownloadFilters(oExtensionAPI, mOptions);
+        var aFilters = _buildMailSenderFilters(oExtensionAPI, mOptions);
         // Non si leggono le righe HTML gia caricate in tabella:
         // si rimandano al backend i filtri attivi, cosi il dataset e completo anche con paging server-side.
-        var oListBinding = oModel.bindList("/PdfDownload", undefined, undefined, aFilters, {
-            $select: "Attachment,FileName,MimeType"
+        var oListBinding = oModel.bindList("/MailSender", undefined, undefined, aFilters, {
+            $select: "AgentCode,AgentName,StatusCode,LogMessage,ProcessedObj"
         });
         var aContexts = await oListBinding.requestContexts(0, 1);
         var oContext;
@@ -80,24 +126,22 @@ sap.ui.define([
 
         if (!aContexts.length) {
             throw new Error("Nessun dato trovato per i filtri selezionati.");
-        } else if (aContexts.length < 0) {
-            throw new Error("Errore durante la generazione del PDF.");
+        } else {
+//            throw new Error("Errore durante la generazione del PDF.");
         }
 
         oContext = aContexts[0];
         oResult = oContext.getObject();
 
-        if (oResult && oResult.Attachment) {
-            oBlob = _base64ToBlob(oResult.Attachment, oResult && oResult.MimeType);
-        } else {
-            oBlob = await _downloadAttachmentStream(oContext, oResult);
-        }
+//        if (oResult && oResult.Attachment) {
+//            oBlob = _base64ToBlob(oResult.Attachment, oResult && oResult.MimeType);
+//        } else {
+//            oBlob = await _downloadAttachmentStream(oContext, oResult);
+//        }
 
         return {
-            blob: oBlob,
-            fileName: (oResult && oResult.FileName) || "CommissionsAccrued.pdf"
+            log: oResult
         };
---- Da verificare -------------------------------------------------- */      
     }
 
     return {
@@ -106,14 +150,12 @@ sap.ui.define([
         runReportPdfSending: async function (oExtensionAPI) {
 //          var userLang = sap.ui.getCore().getConfiguration().getLanguage();
 //                         'it-IT'  'en-US'  'de-DE'  'fr-FR'  'es-ES'
-            var mOptions = await _openPrintOptionsDialog(oExtensionAPI);
+            var mOptions = await _openSendOptionsDialog(oExtensionAPI);
             var oPdf;
-/*  Da verificare --------------------------------------------------
             if (!mOptions) {
                 return;
             }
----  Da verificare -------------------------------------------------- */
-            oPdf = await _sendPdfFromListReport(oExtensionAPI, mOptions);
+            oPdf = await _sendMailFromListReport(oExtensionAPI, mOptions);
 /*  Da verificare --------------------------------------------------
             await _sendingdBlob(oPdf.blob, oPdf.fileName);
 ---  Da verificare -------------------------------------------------- */            
